b0a5dc8a15cbd6900df99e01f1f9d0083d997a7e
Jingyi Wang <jingyiwang@Jingyis-MacBook-Pro.local>
2017-09-06 11:58:29 -0700

    Fix a problem of route handler for the path of index.html

--- index.js
+++ index.js
@@ -40,11 +40,14 @@ require('./routes/billingRoutes')(app);
 // authRoutes(app);
 
 if(process.env.NODE_ENV === 'production'){
+    // The order is important!
     // Express will serve up production assets like our main.js file or main.css file under client/build/static dir
     app.use(express.static('client/build'));
     // Express will serve up the index.html file if it doesn't recognize the route (from react router)
     const path = require('path');
-    res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    });
 }
 
 
